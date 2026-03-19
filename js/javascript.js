541d77074faa7aa732092d282dfbf44c74b28164
Min Naing Oo <minnaingoo792@gmail.com>
2026-03-20 00:45:41 +0630

    secret-photo-update

--- js/javascript.js
+++ js/javascript.js
@@ -1,26 +1,15 @@
-// function checkPass(event){
-//    event.preventDefault();
-//         const input = document.getElementById('user-pass').value;
-//     const key = "231020";
-//     if (input === key) {
-//         document.getElementById('form-box').style.display="none";
-//         document.getElementById('show-photo').style.display="block";
-//     } else {
-//         document.getElementById('user-pass').value ="";
-//         alert("Access denied !")
-//     }
-// }
+
 function checkPass() {
     
     const input = document.getElementById('user-pass').value;
-    const secretKey = "231020";
+    const secretKey = "111111";
     if (input === secretKey) {
         document.getElementById('secret-gallery').style.display="block";
         document.getElementById('login-box').style.display="none";
         // alert("Correct !!!");
     } else {
         document.getElementById('user-pass').value ="";
-        alert("Wrong password !!! Reenter Password");
+        alert("Password မှားနေပါတယ်။ ပြန်လည်ကြိုးစားပါ။");
         document.getElementById('user-pass').focus();
        
     }
